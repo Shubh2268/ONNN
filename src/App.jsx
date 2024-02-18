89f9984a1fc070e4c3419bd2092500dee5125075
Shubh <shubh2268@gmail.com>
2024-02-18 20:27:02 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Navbar = () => {
+
+  const links = [
+    {
+      id: 1,
+      link: 'home'
+    },
+    {
+      id: 2,
+      link: 'services'
+    },
+    {
+      id: 3,
+      link: 'about'
+    },
+    {
+      id: 4,
+      link: 'support'
+    },
+    {
+      id: 5,
+      link: 'pricing'
+    }
+  ];
+
+  return (
+    <div className='w-full h-15 z-20 fixed bg-gray-900 text-white'>
+      <div className='px-3 flex items-center justify-between w-full h-full'>
+        <div className='flex items-center'>
+          <h1 className='text-4xl font-bold'>onnn</h1>
+        </div>
+
+        <ul className='flex'>
+          {
+            links.map(({ id, link }) => (
+              <li key={id} className='capitalize text-lg p-4 cursor-pointer text-gray-200 hover:text-lightColor duration-200'>
+                <a>
+                  {link}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
