7ecb0cca28743082124d2acb962e3c141c96514c
Shubh <shubh2268@gmail.com>
2024-02-19 17:39:45 +0530

    --

--- src/components/GreenButton.jsx
+++ src/components/GreenButton.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const GreenButton = ({ title, className }) => {
-  return (
-    <button
-      className={'border bg-lightColor text-darkColor border-lightColor hover:bg-transparent hover:text-lightColor rounded-md duration-300 ease-out' +
-        ' ' + className} >{title}
-    </button>
-  )
+    return (
+        <button className={'border bg-lightColor text-darkColor border-lightColor hover:bg-transparent hover:text-lightColor rounded-md duration-300 ease-out' + ' ' + className} >{title}
+        </button>
+    )
 }
 
 export default GreenButton;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LiaTimesSolid } from "react-icons/lia";
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import GreenButton from './GreenButton';
 
 const Navbar = () => {
+
+  const [navigation, setNavigation] = useState(false);
 
   const links = [
     {
@@ -27,15 +32,17 @@ const Navbar = () => {
 
   return (
     <div className='w-full h-15 z-20 fixed bg-gray-900 text-white'>
+
       <div className='px-3 flex items-center justify-between w-full h-full'>
+
         <div className='flex items-center'>
-          <h1 className='text-4xl font-bold'>onnn</h1>
+          <h1 className='text-3xl font-bold'>ONNN</h1>
         </div>
 
-        <ul className='flex'>
+        <ul className='hidden sm:flex'>
           {
             links.map(({ id, link }) => (
-              <li key={id} className='capitalize text-lg p-4 cursor-pointer text-gray-200 hover:text-lightColor duration-200'>
+              <li key={id} className='capitalize text-lg font-medium p-4 cursor-pointer text-gray-100 hover:text-lightColor duration-200'>
                 <a>
                   {link}
                 </a>
@@ -44,11 +51,51 @@ const Navbar = () => {
           }
         </ul>
 
-        <div className='flex mr-4'>
-          <button type='btn' className='capitalize px-9 py-3'>Sign In</button>
+        <div className='hidden sm:flex mr-4'>
+          <GreenButton title='sign in' className='capitalize font-medium px-9 py-3' />
+        </div>
+
+        {/* burger icon */}
+        <div className='sm:hidden'>
+          <div className='cursor-pointer'>
+            <HiBars3BottomLeft onClick={() => setNavigation(true)} size={25} />
+          </div>
         </div>
 
       </div>
+
+      {/* mobile menu  */}
+      <div className={navigation ? 'sm:hidden fixed left-0 top-0 w-full h-full bg-gray-900/70 backdrop-blur' : ''}>
+        <div className={navigation ? 'fixed left-0 top-0 w-4/5 h-full bg-gray-900 text-white p-10 ease-in duration-500' : 'fixed top-0 left-[-100%] h-full p-10 duration-500 ease-in'}>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <h2 onClick={() => setNavigation(false)} className='text-3xl font-bold capitalize cursor-pointer'>onnn</h2>
+              <div className='p-3 cursor-pointer'>
+                <LiaTimesSolid onClick={() => setNavigation(false)} size={25} />
+              </div>
+            </div>
+          </div>
+
+          <div className='mt-6 flex flex-col h-fit gap-12'>
+            <GreenButton className='px-9 py-3 capitalize' title='sign in' />
+
+            <ul className='capitalize'>
+              {
+                links.map(({ id, link }) => (
+                  <li key={id} className='py-2 text-xl tracking-wide cursor-pointer'>
+                    <a>
+                      {link}
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+
+            
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
